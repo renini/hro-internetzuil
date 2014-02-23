2d59604a8f84b8b962956cff4e3372d176668d4f
renini <renini@gmail.com>
2014-02-23 18:57:29 +0100

    disable escape key for exit fullscreen

--- main.js
+++ main.js
@@ -4,5 +4,16 @@
 
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create('browser.html', {state: "fullscreen"}, function(win) {
+    // The following key events handler will prevent the default behavior for
+    // the ESC key, thus will prevent the ESC key to leave fullscreen.
+    win.contentWindow.document.addEventListener('keydown', function(e) {
+	  if (e.keyCode == 27) // 'ESC'
+      e.preventDefault();
+    });
+    win.contentWindow.document.addEventListener('keyup', function(e) {
+	  if (e.keyCode == 27) // 'ESC'
+      e.preventDefault();
+    });
+	
   });
 });
