ddf6b2b1946a79268c00fdf09a7757f06f444f41
renini <renini@gmail.com>
2014-02-23 18:56:08 +0100

    slim down main.js

--- main.js
+++ main.js
@@ -1,33 +1,8 @@
-/**
- * Listens for the app launching then creates the window
- *
- * @see http://developer.chrome.com/apps/app.runtime.html
- * @see http://developer.chrome.com/apps/app.window.html
- */
+// Copyright 2014 Rene Arends. All rights reserved.
+// Hogeschool Rotterdam
+// HR InternetZuil
+
 chrome.app.runtime.onLaunched.addListener(function() {
-  runApp();
-});
-
-/**
- * Listens for the app restarting then re-creates the window.
- *
- * @see http://developer.chrome.com/apps/app.runtime.html
- */
-chrome.app.runtime.onRestarted.addListener(function() {
-  runApp();
-});
-
-/**
- * Creates the window for the application.
- *
- * @see http://developer.chrome.com/apps/app.window.html
- */
-function runApp() {
-  chrome.app.window.create('browser.html', {
-  	id: "browserWinID",
-    bounds: {
-      'width': 1024,
-      'height': 768
-    }
+  chrome.app.window.create('browser.html', {}, function(win) {
   });
-}
+});
